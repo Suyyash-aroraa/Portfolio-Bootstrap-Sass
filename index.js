6cbe17faee71c7f780dff81fdaed3b2db23a426b
Suyyash_Ar <suyyasha@gmail.com>
2023-07-12 14:23:18 +0530

    contact completed

---
+++ index.js
@@ -0,0 +1,6 @@
+document.querySelector("#button-portfolio-ui").addEventListener('click', () => {
+    alert('Sorry currently UI/UX services are on hold.')
+})
+document.querySelector("#learn-button-ui").addEventListener('click', () => {
+    alert('Sorry currently UI/UX services are on hold.')
+})
